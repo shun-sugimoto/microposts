e0bf2bc57eb64325734cfb5f15dfee454a7f2560
shun_sugimoto <shun.sugimoto.kaisya@gmail.com>
2016-03-15 09:22:58 +0000

    add home view's timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
     has_many :following_relationships, class_name: "Relationship",foreign_key: "follower_id", dependent: :destroy
     has_many :following_users, through: :following_relationships, source: :followed
     has_many :follower_relationships, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
-    has_many :following_users, through: :following_relationships, source: :follower
+    has_many :follower_users, through: :follower_relationships, source: :follower
 
 
     # 他のユーザをフォローする
@@ -27,5 +27,11 @@ class User < ActiveRecord::Base
     # あるユーザをフォローしているかどうか?
     def following?(other_user)
         following_users.include?(other_user)
+    end
+
+
+    def feed_items
+        Micropost.where(user_id: following_user_ids + [self.id])
+
     end
 end
